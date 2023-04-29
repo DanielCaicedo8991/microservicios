61fa52eac1ca5238cf2050e877711dc6e810d15e
danielcaicedo98 <daniel.andres.caicedo@correounivalle.edu.co>
2023-04-28 19:39:26 -0500

    subiendo chat

---
+++ index.js
@@ -0,0 +1,43 @@
+// io.on('connection', (socket) => {
+//     console.log('Se ha conectado un cliente');
+    
+
+//     socket.broadcast.emit('chat_message', {
+//         usuario: 'INFO',
+//         mensaje: 'Se ha conectado un nuevo usuario'
+//     });
+
+//     socket.on('chat_message', (data) => {
+//         io.emit('chat_message', data);
+//         console.log(data)
+//     });
+// });
+
+// server.listen(3000);
+const http = require('http');
+
+const server = http.createServer();
+
+const io = require('socket.io')(server, {
+    cors: { origin: '*' }
+});
+
+io.on('connection', (socket) => {
+    console.log('Se ha conectado un cliente');
+  
+    if(socket.id === 'admin') {
+      socket.emit('chat_message', {
+        usuario: 'INFO',
+        mensaje: 'Bienvenido al chat de administrador'
+      });
+    }
+  
+    socket.join('admin');
+  
+    socket.on('chat_message', (data) => {
+      io.to('admin').emit('chat_message', data);
+      console.log(data)
+    });
+  });
+  
+server.listen(3000);  
